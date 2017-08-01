5e7ea8441040e5a38c1c57dddb1414ea91cf8a1a
EvilStar <rsx_game@163.com>
2017-08-01 23:11:33 +0800

    显示天气到界面【2】

--- app/src/main/java/com/morningstar/evilweather/activity/WeatherActivity.java
+++ app/src/main/java/com/morningstar/evilweather/activity/WeatherActivity.java
@@ -4,13 +4,24 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.morningstar.evilweather.R;
+import com.morningstar.evilweather.gson.Forecast;
 import com.morningstar.evilweather.gson.Weather;
+import com.morningstar.evilweather.util.HttpUtil;
 import com.morningstar.evilweather.util.Utility;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 /**
  * Created by morningstar on 2017/7/30.
@@ -37,7 +48,51 @@ public class WeatherActivity extends AppCompatActivity {
             //有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
             showWeatherInfo(weather);
+        }else {
+            //无缓存时去服务器查询天气
+            String weatherId = getIntent().getStringExtra("weather_id");
+            mWeatherLayout.setVisibility(View.INVISIBLE);
+            requestWeather(weatherId);
         }
+    }
+
+    /**
+     * 根据天气 id 请求城市天气信息
+     * @param weatherId 天气 id
+     */
+    private void requestWeather(String weatherId) {
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=2b5a6884508e43c3885293b7f217f8d1";
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(WeatherActivity.this, "获取天气信息失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                final Weather weather = Utility.handleWeatherResponse(responseText);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(weather != null && "ok".equals(weather.status)){
+                            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                            editor.putString("weather",responseText);
+                            editor.apply();
+                            showWeatherInfo(weather);
+                        } else {
+                            Toast.makeText(WeatherActivity.this,"获取天气信息失败",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     /**
@@ -45,7 +100,38 @@ public class WeatherActivity extends AppCompatActivity {
      * @param weather 天气数据
      */
     private void showWeatherInfo(Weather weather) {
-        
+        String cityName = weather.basic.cityName;
+        String updateTime = weather.basic.update.updateTime.split(" ")[1];
+        String degree = weather.now.temperature + "℃";
+        String weatherInfo = weather.now.more.info;
+        mTitleCity.setText(cityName);
+        mTitleUpdateTime.setText(updateTime);
+        mDegreeText.setText(degree);
+        mWeatherInfoText.setText(weatherInfo);
+        mForecastLayout.removeAllViews();
+        for (Forecast forecast : weather.forecastList) {
+            View view = LayoutInflater.from(this).inflate(R.layout.forecast_item, mForecastLayout, false);
+            TextView dateText = (TextView) view.findViewById(R.id.date_text);
+            TextView infoText = (TextView) view.findViewById(R.id.info_text);
+            TextView maxText = (TextView) view.findViewById(R.id.max_text);
+            TextView minText = (TextView) view.findViewById(R.id.min_text);
+            dateText.setText(forecast.date);
+            infoText.setText(forecast.more.info);
+            maxText.setText(forecast.temperature.max);
+            minText.setText(forecast.temperature.min);
+            mForecastLayout.addView(view);
+        }
+        if (weather.aqi != null) {
+            mAqiText.setText(weather.aqi.city.aqi);
+            mPM25Text.setText(weather.aqi.city.pm25);
+        }
+        String comfort = "舒适度：" + weather.suggestion.comfort.info;
+        String carWash = "洗车指数：" + weather.suggestion.carWash.info;
+        String sport = "运动建议：" + weather.suggestion.sport.info;
+        mComfortText.setText(comfort);
+        mCarWashText.setText(carWash);
+        mSportText.setText(sport);
+        mWeatherLayout.setVisibility(View.VISIBLE);
     }
 
     private void initViews() {
